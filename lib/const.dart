f0189549a675c13df2f768964cac827e2920d85e
sureshturbo <sureshrajanklnmca@gmail.com>
2023-02-16 22:11:38 +0530

    first commit

---
+++ lib/const.dart
@@ -0,0 +1,1 @@
+const apiSecretKey = "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";
---
+++ lib/model.dart
@@ -0,0 +1,11 @@
+enum ChatMessageType { user, bot}
+
+class ChatMessage {
+late final String text;
+  late final ChatMessageType chatMessageType;
+
+  ChatMessage({
+   required this.chatMessageType,
+    required this.text
+});
+}
